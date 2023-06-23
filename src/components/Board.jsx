560999ae056a1308c3ad4c81175d5fb8c20c929c
vikasrawat29180 <vikasrawat29180@gmail.com>
2023-06-23 20:11:56 +0530

    introduced a player in agame when clicking on square show either X or O

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,55 +1,50 @@
 import Square from './Square';
-import {useState} from'react';
+import { useState } from 'react';
 const Board = () => {
-    const [squares,setSquares]=useState(Array(9).fill(null));
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isNext, setIsXNext] = useState(false);
 
-    const handleSquareClick= clickedPosition=>{
-        
-     setSquares(currentSquares=>{
-return currentSquares.map((SquareValue,position)=>{
-if(clickedPosition===position){
-    return'X';
-}
-return SquareValue;
-      
-});
+  const handleSquareClick = clickedPosition => {
+    if (squares[clickedPosition]) {
+      return;
+    }
 
+    setSquares(currentSquares => {
+      return currentSquares.map((SquareValue, position) => {
+        if (clickedPosition === position) {
+          return isNext ? 'X' : 'O';
+        }
+        return SquareValue;
+      });
+    });
+    setIsXNext(currentIsXNext => !currentIsXNext);
+  };
 
-
-     })   ;
-
-
-    };
-
-
-    const renderSquare=position=>{
-return(
-<Square
-value={squares[position]}
-onClick={()=> handleSquareClick(position)}
-/>
-);
-    };
-
+  const renderSquare = position => {
+    return (
+      <Square
+        value={squares[position]}
+        onClick={() => handleSquareClick(position)}
+      />
+    );
+  };
 
   return (
     <div className="board">
       <div className="board-row">
-       {renderSquare(0)}
-       {renderSquare(1)}
-       {renderSquare(2)}
-
-        
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-      {renderSquare(3)}
-      {renderSquare(4)}
-      {renderSquare(5)}
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-      {renderSquare(6)}
-      {renderSquare(7)}
-      {renderSquare(8)}
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
